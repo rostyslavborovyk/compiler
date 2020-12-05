4be2619c2bc947a2a401ca775240bfa27be5635d
rost <rostmoguchiy@gmail.com>
2020-12-05 13:14:39 +0200

    added arch variation (32 and 64 bits)

--- compiler.py
+++ compiler.py
@@ -27,10 +27,19 @@ arg_parser.add_argument(
     metavar="output",
     help="path of output file with .asm extension",
 )
+arg_parser.add_argument(
+    "--arch",
+    dest="arch",
+    nargs="?",
+    default=32,
+    type=int,
+    metavar="arch",
+    help="architecture for assembler (32 or 64)",
+)
 
 
-def compile_to_exec(cpp_path, exec_path):
-    subprocess.run(f"g++ -masm=intel {cpp_path} -o {exec_path}".split(" "))
+def compile_to_exec(cpp_path, exec_path, arch):
+    subprocess.run(f"g++ -m{arch} -masm=intel {cpp_path} -o {exec_path}".split(" "))
     # p = subprocess.Popen([f"./{exec_path}"], stdin=subprocess.PIPE, stdout=subprocess.PIPE,
     #                      stderr=subprocess.PIPE)
     # output, err = p.communicate()
@@ -39,7 +48,7 @@ def compile_to_exec(cpp_path, exec_path):
     # print(res)
 
 
-def compiler(text, system_arch, output_path=None, test=False):
+def compiler(text, arch, output_path=None, test=False):
     lexer = Lexer(text)
     tokens = lexer.get_tokens()
     # pprint(tokens)
@@ -47,8 +56,8 @@ def compiler(text, system_arch, output_path=None, test=False):
     ast = parser.parse()
     # ast.prettyAST()
     interpreter = Interpreter(ast)
-    interpreter.interpret(output_path, test, system_arch)
-    compile_to_exec("output.cpp", "output.exe")
+    interpreter.interpret(output_path, test, arch)
+    compile_to_exec("output.cpp", "output.exe", arch)
 
 
 def main():
@@ -56,13 +65,16 @@ def main():
     src = args.src.name
     if src[-3:] != ".py":
         raise ValueError("--src file should have .py extension")
+
     asm_out = args.asm_out.name
     if asm_out[-4:] != ".asm":
         raise ValueError("--output file should have .asm extension")
 
+    arch = args.arch
+
     with open(src, "rb") as f:
         program_text = str(f.read())[2:-1]  # trims b"str" to str
-    compiler(program_text, output_path=asm_out, system_arch=32)
+    compiler(program_text, output_path=asm_out, arch=arch)
 
 
 if __name__ == "__main__":
